9459bf06e7fd062dd6f936bd5e71dadfbbf67261
Samson Ameh <amehsam69@gmail.com>
2023-12-04 04:17:57 +0100

    renderMenu readjusted

--- index.js
+++ index.js
@@ -1,37 +1,70 @@
 import { menuArray } from "./data.js";
 
+const menuSection = document.querySelector(".menu-section");
+const checkoutSection = document.querySelector(".checkout-section");
+
+const selectedOrders = [];
+
 document.addEventListener("DOMContentLoaded", () => {
-  getMenuHTML();
+  renderMenu();
 });
 
 document.addEventListener("click", (e) => {
-  if (e.target.dataset.itemName) {
-    // getCheckoutHTML();
+  if (e.target.dataset.itemId) {
+    const itemId = e.target.dataset.itemId;
+    const selectedItem = menuArray.find((item) => item.id === itemId);
+
+    if (selectedItem) {
+      selectedOrders.push({
+        name: selectedItem.name,
+        price: selectedItem.price,
+      });
+      renderSelectedOrder();
+    }
   }
 });
 
-function getMenuHTML() {
+function renderMenu() {
   const htmlArray = menuArray.map(
     ({ name, ingredients, id, price, emoji }) => `
-    <div class="menu-item-container">
-      <p class="menu-item-img">${emoji}</p>
+    <div class="menu-item-container" data-item-id="${id}">
+    <p class="menu-item-img">${emoji}</p>
       <div class="menu-item-content">
         <h2 class="menu-item-title">${name}</h2>
         <p class="menu-item-description">${ingredients}</p>
-        <h3 class="menu-item-price">$ ${price}</h3>
+        <h3 class="menu-item-price">${price}</h3>
       </div>
       <div>
-        <button class="menu-item-add-btn" data-item-name="${id}">+</button>
+        <button class="menu-item-add-btn" data-item-id="${id}">+</button>
       </div>
-    </div>`
+    </div>
+    `
   );
 
-  const menuSection = document.createElement("section");
-  menuSection.classList.add("menu-section");
-  const container = document.createElement("div");
-  container.classList.add("container");
-  container.innerHTML = htmlArray.join("");
-  menuSection.appendChild(container);
+  menuSection.innerHTML = htmlArray.join("");
+}
 
-  document.body.appendChild(menuSection);
+function renderSelectedOrder() {
+  const htmlArray = selectedOrders.map(
+    ({ name, price }) =>
+      `
+    <div class="checkout-inner-container">
+      <div class="checkout-inner-left">
+        <h2 class="checkout-item-name">${name}</h2>
+        <button class="checkout-remove-item-btn">remove</button>
+      </div>
+      <h3 class="checkout-item-price">${price}</h3>
+    </div>
+  `
+  );
+
+  const checkoutContainer = `
+    <div class="checkout-container">
+      <h2 class="checkout-title">Your order</h2>
+      ${htmlArray.join("")}
+      <button class="purchase-btn">Complete order</button>
+    </div>
+  `;
+
+  checkoutSection.innerHTML = checkoutContainer;
 }
